eaf2d3be13a6bfe4c2eb16ca7f2f71a664863c1a
tjodalv2k <svennsorensen@gmail.com>
2017-09-07 16:33:15 +0200

    Powershell scripts

---
+++ Powershell/task1-4.ps1
@@ -0,0 +1,45 @@
+﻿#Mandatory Powershell assignment, 1-4.
+
+#The Encrypted string says 'Keyboard not found. Press F1 to continue'
+
+function decrypt($char){
+
+    $number = [int][char]$char
+    $number = $number - 1;
+
+    return [char]$number
+}
+
+function encrypt($char){
+
+    $number = [int][char]$char
+    $number = $number +1;
+
+    return [char]$number
+}
+
+function decryption($string){
+    
+    $string_list = $string.ToCharArray()
+    $result_list = foreach($char in $string_list){
+        decrypt($char) 
+
+    }
+
+    $result = [string]$result_list
+    return $result
+}
+
+function encryption($string){
+    
+    $string_list = $string.ToCharArray()
+    $result_list = foreach($char in $string_list){
+        encrypt($char)
+    }
+
+    return [string]$result_list
+}
+
+decryption -string 'Lfzcpbse!opu!gpvoe/!Qsftt!G2!up!dpoujovf'
+
+encryption -string 'Keyboard not found. Press F1 to continue'
